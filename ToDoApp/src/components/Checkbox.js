c863eea79550f3c54aaaa04798b3886f591a3858
G-Ost <g.ostapczuk@gmail.com>
2021-05-29 11:30:52 +0200

    git sync

--- ToDoApp/src/components/Checkbox.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { TaskListReducerContext } from "../contexts/taskListReducerContext"
-
-function Checkbox(props) {
-    let { elementsSizeUnit } = useContext(TaskListReducerContext);
-    return <input onChange={props.onClickBox} type="checkbox" checked={props.isChecked} style={{ transform: `scale(${elementsSizeUnit / 330})` }}></input>
-}
-
-export default Checkbox;
